f515207d29460052e8b5ab7c70ac77359f38f316
DKojwang04 <dkojwang04@gmail.com>
2023-10-17 16:23:26 +0300

    initial commit

---
+++ mycrudapp/src/main/java/ke/technovation/mycrudapp/Service/InventoryItemService.java
@@ -0,0 +1,48 @@
+package ke.technovation.mycrudapp.Service;
+
+import ke.technovation.mycrudapp.model.InventoryItem;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class InventoryItemService {
+    private final List<InventoryItem> inventoryItems = new ArrayList<>();
+    private long idCounter = 1;
+
+    public List<InventoryItem> getAllItems() {
+        return inventoryItems;
+    }
+    // Logic to find and return an item by ID
+    public InventoryItem getItemById(Long id) {
+        return inventoryItems.stream()
+                .filter(item -> item.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+    //Logic to add a new item
+
+    public InventoryItem addItem(InventoryItem item) {
+        item.setId(idCounter++);
+        inventoryItems.add(item);
+        return item;
+    }
+    //Logic to update an existing item
+
+    public InventoryItem updateItem(Long id, InventoryItem item) {
+        for (int i = 0; i < inventoryItems.size(); i++) {
+            if (inventoryItems.get(i).getId().equals(id)) {
+                item.setId(id);
+                inventoryItems.set(i, item);
+                return item;
+            }
+        }
+        return null;
+    }
+    // Logic to delete an item
+
+    public void deleteItem(Long id) {
+        inventoryItems.removeIf(item -> item.getId().equals(id));
+    }
+}
+
---
+++ mycrudapp/src/main/java/ke/technovation/mycrudapp/controller/InventoryItemController.java
@@ -0,0 +1,44 @@
+package ke.technovation.mycrudapp.controller;
+
+import ke.technovation.mycrudapp.model.InventoryItem;
+import ke.technovation.mycrudapp.Service.InventoryItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryItemController {
+
+    private final InventoryItemService inventoryItemService;
+
+    @Autowired
+    public InventoryItemController(InventoryItemService inventoryItemService) {
+        this.inventoryItemService = inventoryItemService;
+    }
+    //   Delegate the request to the service class and return the response
+    @GetMapping("/")
+    public List<InventoryItem> getAllItems() {
+        return inventoryItemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public InventoryItem getItemById(@PathVariable Long id) {
+          return inventoryItemService.getItemById(id);
+    }
+
+    @PostMapping("/")
+    public InventoryItem addItem(@RequestBody InventoryItem item) {
+        return inventoryItemService.addItem(item);
+    }
+
+    @PutMapping("/{id}")
+    public InventoryItem updateItem(@PathVariable Long id, @RequestBody InventoryItem item) {
+        return inventoryItemService.updateItem(id, item);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteItem(@PathVariable Long id) {
+        inventoryItemService.deleteItem(id);
+    }
+}
